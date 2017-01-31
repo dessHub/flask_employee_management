c442d9b9eacad85ce226631efb62ea7b9fd60b34
dessHub <deskip95@gmail.com>
2017-01-31 22:17:20 +0300

    set up models

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 from config import app_config
 
 # db variable initialization
 db = SQLAlchemy()
+login_manager = LoginManager()
+
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
@@ -13,9 +16,9 @@ def create_app(config_name):
     app.config.from_pyfile('config.py')
     db.init_app(app)
 
-    @app.route('/')
-    def hello_world():
-        return 'Hello, World!'
+    login_manager.init_app(app)
+    login_manager.login_message = "You must be logged in to access this page."
+    login_manager.login_view = "auth.login"
 
 
     return app
