c798b4f22cfb7718161b903108558815230c7da8
dessHub <deskip95@gmail.com>
2017-02-02 00:40:06 +0300

    set up department views

--- app/home/views.py
+++ app/home/views.py
@@ -1,7 +1,9 @@
 from flask import abort, render_template
 from flask_login import current_user, login_required
+from ..models import Department, Employee, Role
 
 from . import home
+from .. import db
 
 @home.route('/')
 def homepage():
@@ -22,8 +24,43 @@ def dashboard():
 @home.route('/admin/dashboard')
 @login_required
 def admin_dashboard():
-    # prevent non-admins from accessing the page
     if not current_user.is_admin:
         abort(403)
 
     return render_template('home/admin_dashboard.html', title="Dashboard")
+
+# Department View
+
+# prevent non-admins from accessing the page
+@home.route('/departments', methods=['GET', 'POST'])
+def list_departments():
+    """
+    List all departments
+    """
+
+    departments = Department.query.all()
+
+    return render_template('home/departments/departments.html',
+                           departments=departments, title="Departments")
+
+@home.route('/departments/view/<int:id>', methods=['GET', 'POST'])
+def list_department_employees(id):
+    """
+    Display department's employees
+    """
+    employees = Employee.query.filter_by(department_id=id).all()
+    department = Department.query.get_or_404(id)
+    print employees
+    return render_template('home/departments/department.html', employees=employees, department=department, itle="Department Employees")
+
+# Employee Views
+
+@home.route('/employees')
+def list_employees():
+    """
+    List all employees
+    """
+    employees = Employee.query.all()
+    print employees
+    return render_template('home/employees/employees.html',
+                           employees=employees, title='Employees')
